bff404fc428243b4eca7cf0a53d6d01329e43d29
Gillespie <gillemi@9801a79dae39.ant.amazon.com>
2017-01-18 12:11:39 -0600

    added each month to shell

--- upload.sh
+++ upload.sh
@@ -1,3 +1,35 @@
-node index.js --lastyear
+node index.js --lastyear 01
 aws s3 sync data/ s3://mjg-master-builder/ingestion
-
+sleep 20
+node index.js --lastyear 02
+aws s3 sync data/ s3://mjg-master-builder/ingestion
+sleep 20
+node index.js --lastyear 03
+aws s3 sync data/ s3://mjg-master-builder/ingestion
+sleep 20
+node index.js --lastyear 04
+aws s3 sync data/ s3://mjg-master-builder/ingestion
+sleep 20
+node index.js --lastyear 05
+aws s3 sync data/ s3://mjg-master-builder/ingestion
+sleep 20
+node index.js --lastyear 06
+aws s3 sync data/ s3://mjg-master-builder/ingestion
+sleep 20
+node index.js --lastyear 07
+aws s3 sync data/ s3://mjg-master-builder/ingestion
+sleep 20
+node index.js --lastyear 08
+aws s3 sync data/ s3://mjg-master-builder/ingestion
+sleep 20
+node index.js --lastyear 09
+aws s3 sync data/ s3://mjg-master-builder/ingestion
+sleep 20
+node index.js --lastyear 10
+aws s3 sync data/ s3://mjg-master-builder/ingestion
+sleep 20
+node index.js --lastyear 11
+aws s3 sync data/ s3://mjg-master-builder/ingestion
+sleep 20
+node index.js --lastyear 12
+aws s3 sync data/ s3://mjg-master-builder/ingestion
